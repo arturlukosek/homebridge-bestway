786880df89b7baa032602bafa699bbea4914f270
AnthonyAnise <32623623+AnthonyAnise@users.noreply.github.com>
2022-12-09 21:34:08 -0500

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
-export const PLATFORM_NAME = 'Lay-Z-Spa'
-export const PLUGIN_NAME = 'homebridge-lay-z-spa'
+export const PLATFORM_NAME = 'Bestway'
+export const PLUGIN_NAME = 'homebridge-bestway-spa'
